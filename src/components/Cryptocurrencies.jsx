f5c41de755686b51a35280f959ea88e0b8ba89ec
sambara323 <sambara323@gmail.com>
2021-09-20 14:39:46 +0000

    Initil D

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = ({simplified}) => {
-  const count = simplified ? 10 :100;
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setcryptos] = useState(cryptosList?.data?.coins);
-  if(isFetching) return "Loading...";
+  const [cryptos, setcryptos] = useState([]);
+  const [searchTerm, setsearchTerm] = useState("");
 
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm)
+    );
+
+    setcryptos(filteredData);
+  }, [cryptosList, searchTerm]);
+
+  if (isFetching) return "Loading...";
 
   return (
     <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrencies"
+            onChange={(e) => setsearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
